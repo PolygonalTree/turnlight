edd34a1e0c2ac4c0b64994b973a2fa4f619a9e0a
pepelisu <pepelisu@gmail.com>
2013-10-31 15:46:33 +0000

    Add licence
    Add licence in files

--- left_turnlight/left_turnlight.ino
+++ left_turnlight/left_turnlight.ino
@@ -1,4 +1,24 @@
-// made by Kristian Lauszus - see http://arduino.cc/forum/index.php/topic,58048.0.html for information
+// Made by Kristian Lauszus - see http://arduino.cc/forum/index.php/topic,58048.0.html for information
+// Modified by Polygonal Tree (Luis Garcia Rodriguez) to adjust the paramters and filter for the TurnLight for cyclist, for more info: http://polygonaltree.co.uk/wiki/opensource:turnlight
+/*
+   Copyright (C) 2013  Kristian Lauszus
+   Copyright (C) 2013  Luis García Rodríguez <dirección de correo>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #define gX A1
 #define gY A2
 #define gZ 
